694b7bd61376ee0871adfe5c4d06be48b55c6881
Eliezer Silva <djosacv@gmail.com>
2015-04-26 06:41:10 -0300

    implementacao do VoronoiLSH

--- hashtable.py
+++ hashtable.py
@@ -9,7 +9,7 @@ class Lshtable:
 		self.table = Series(l:Series({k:[] for k in range(self.numk)}) for l in range(self.numT)})
 	
 	def mapindex(self,key):
-		vecfun=np.vectorize(lambda f:f(key))
+		vecfun=np.vectorize(lambda f:f.hash(key))
 		return vecfun(self.f)
 
 	def indexdata(self):
@@ -34,11 +34,36 @@ class LSHkNN:
 	def __init__(self,distfun,lshtble):
 		self.dist = distfun
 		self.table = lshtble
-	
-	def kNNQuery(self,k,query):
+	def candidateDistance(self,query):
 		fcalcdist=lambda ind:(self.dist(self.table.data[ind],query),ind)
-		distlst = [fcalcdist(i) for i in self.table.candidateset(query)]
-		distlst.sort(reverse=True)
+		return [fcalcdist(i) for i in self.table.candidateset(query)]
+	def kNNQuery(self,k,query):
+	        distlst = candidateDistance(query)
+		distlst.sort()
 		return distlst[:k]
+	def rangeQuery(self,radi,query):
+		distlst = candidateDistance(query)
+		return [(x,y) if x < radi for (x,y) in distlst]
 
-		
+class VoronoiLSH:
+	def __init__(self,anchorpoints,dist):
+		self.dist=dist
+		self.C = anchorpoints
+	def hash(self,q):
+		fdist=np.vectorize(lambda x:self.dist(x,q))
+		return fdist(self.C).argmin()
+
+class E2LSH:
+	def __init__(self,d,w,m):
+		self.d = d
+		self.w = w
+		self.m = m
+
+class CossineLSH:
+	def __init__(self,d,numplanes,m):
+		self.d = d
+		self.b = numplanes
+		self.m = m
+
+
+
