15817a34809310d14b8baf7f3c54578ef147d0d6
Alan Godoy Souza Mello <amello@cpqd039042.aquarius.cpqd.com.br>
2015-05-19 14:09:32 -0300

    Incorporando a parte de biometria

--- biometry.py
+++ biometry.py
@@ -2,6 +2,14 @@
 import bz2
 import csv
 import numpy as np
+import sklearn.metrics as mtr
+import sys
+import time
+import matplotlib.pyplot as plt
+
+import hashtable
+
+N_COHORTS = 500
 
 def load_model_key(filename, model_ids):
     """Loads a model key to match order of model_ids
@@ -62,6 +70,171 @@ def load_ivectors(filename):
 
     return ids, np.array(durations, dtype=np.float32), np.vstack(ivectors)
 
+def z_norm(scores, avg_model_ivec, avg_model_names, model_ivec, model_key,
+           n_norm=-1, use_lsh=True):
+    
+    if n_norm > 0 and use_lsh:
+        knn = hashtable.LSHkNN.create_voronoi_lsh(int(np.ceil(n_norm / 50.)), 150, model_ivec)
+    
+    model_key_aux = np.array(model_key)
+    
+    for i_model, (key, model) in enumerate(zip(avg_model_names,
+                                               avg_model_ivec)):
+        if n_norm > 0:
+            if use_lsh:
+                z_distances, similar_trials = knn.knn_query(n_norm, model)
+                is_not_self = (model_key_aux[similar_trials] != key)
+                z_scores = 1 - z_distances[is_not_self]
+            else:
+                is_not_self = (model_key_aux != key)
+                z_scores = np.dot(model_ivec[is_not_self],
+                                  model[:, np.newaxis])[:, 0]
+                z_scores = - np.sort(- z_scores)[:n_norm]
+        else:
+            is_not_self = (model_key_aux != key)
+            z_scores = np.dot(model_ivec[is_not_self],
+                              model[:, np.newaxis])[:, 0]
+        
+        z_mean = np.mean(z_scores)
+        z_std = np.std(z_scores)
+        
+        if z_std == 0:
+            z_std = 1
+        
+        scores[:, i_model] -= z_mean
+        scores[:, i_model] /= z_std
+
+def t_norm(scores, avg_model_ivec, test_ivec, use_t_norm_cohorts=False,
+           use_t_norm_rank=False, t_norm_record_similar=False, n_norm=-1,
+           use_lsh=True):
+    
+    if t_norm_record_similar:
+        similar = np.empty((test_ivec.shape[0], 5), dtype=np.int)
+    
+    if n_norm > 0:
+        # Use LSH to perform t-normalization
+        if use_t_norm_cohorts:
+            if use_lsh:
+                knn = hashtable.LSHkNN.create_voronoi_lsh(10, 20,
+                                                          avg_model_ivec)
+            
+            cohorts = np.empty((avg_model_ivec.shape[0], N_COHORTS),
+                               dtype=np.int)
+            
+            for i_model, model in enumerate(avg_model_ivec):
+                if use_lsh:
+                    _, similar_models = zip(*knn.knn_query(N_COHORTS + 1,
+                                                           model))
+                else:
+                    similar_models = np.argsort(np.dot(avg_model_ivec,
+                                                       model[:, np.newaxis])
+                                                [:, 0])[:N_COHORTS + 1]
+                
+                cohorts[i_model] = \
+                    filter(lambda i: i != i_model, similar_models)[:N_COHORTS]
+        elif use_lsh:
+            knn = hashtable.LSHkNN.create_voronoi_lsh(int(np.ceil(n_norm / 50.)), 25, avg_model_ivec)
+    
+    for i_data, data in enumerate(test_ivec):
+        if n_norm > 0 and use_t_norm_cohorts:
+            t_scores = np.dot(test_ivec[np.newaxis, i_data],
+                              avg_model_ivec[cohorts].transpose())
+            
+            if use_t_norm_rank:
+                scores[i_data] = (scores[i_data][:, np.newaxis] >=
+                                  t_scores).mean(axis=1)
+            else:
+                t_mean = np.mean(t_scores, axis=1)
+                t_std = np.std(t_scores, axis=1)
+                scores[i_data] -= t_mean
+                scores[i_data] /= t_std
+        else:
+            if n_norm > 0:
+                if use_lsh:
+                    t_distances, similar_models = knn.knn_query(n_norm, data)
+                    t_scores = 1 - t_distances
+                else:
+                    t_scores = np.dot(avg_model_ivec, data[:, np.newaxis])[:, 0]
+                    similar_models = np.argsort(- t_scores)[:n_norm]
+                    t_scores = t_scores[similar_models]
+            else:
+                similar_models = range(avg_model_ivec.shape[0])
+                t_scores = np.dot(avg_model_ivec, data[:, np.newaxis])[:, 0]
+            
+            if t_norm_record_similar:
+                similar[i_data] = similar_models[:5]
+            
+            if use_t_norm_rank:
+                scores[i_data] = (scores[i_data][:, np.newaxis] >=
+                                  t_scores[np.newaxis, :]).mean(axis=1)
+            else:
+                t_mean = np.mean(t_scores)
+                t_std = np.std(t_scores)
+                scores[i_data] -= t_mean
+                scores[i_data] /= t_std
+    
+    if t_norm_record_similar:
+        return similar
+
+def generate_models(model_ivec, model_key, m, W):
+    # create a speaker model by taking mean over i-vectors
+    # from that speaker (in this case each should have 5)
+    model_keys = np.unique(model_key)
+    avg_model_ivec = np.zeros((len(model_keys), model_ivec.shape[1]))
+    avg_model_names = []
+    
+    for i, key in enumerate(model_keys):
+        avg_model_ivec[i] = np.mean(model_ivec[model_key == key], axis=0)
+        avg_model_names.append(key)
+    
+    # project the avg model i-vectors into unit sphere
+    avg_model_ivec /= np.sqrt(np.sum(avg_model_ivec ** 2, axis=1)) \
+        [:, np.newaxis]
+    
+    return avg_model_ivec, avg_model_names
+
+def load_trial_keys(filename):
+    with open(filename, "r") as arq:
+        a = [i.strip().split("\t") for i in arq.readlines()[1:]]
+    
+    trial_model_id, trial_segment_id, trial_target, trial_set = zip(*a)
+    trial_model_id = np.array(trial_model_id)
+    trial_segment_id = np.array(trial_segment_id)
+    trial_target = np.array([i == "target" for i in trial_target])
+    trial_set = np.array(trial_set)
+    #trial_is_eval = (trial_set == "eval")
+    
+    return trial_target, trial_set
+
+def eer(scores, labels, trial_set):
+    trial_set_keys = ["eval"]#np.unique(trial_set)
+    
+    for key in trial_set_keys:
+        aux = (trial_set == key)
+        pfa, tpr, thresholds = mtr.roc_curve(labels[aux], scores[aux])
+        pmiss = 1 - tpr
+        i = np.argmin(np.abs(pfa - pmiss))
+        
+        yield key, (pfa[i] + pmiss[i]) / 2., pfa[i], pmiss[i], thresholds[i]
+
+def min_dcf(scores, labels, trial_set):
+    trial_set_keys = ["eval"]#np.unique(trial_set)
+    
+    for key in trial_set_keys:
+        #p_misses(thresh=t) + (100 * fpr(thresh=t))
+        aux = (trial_set == key)
+        fpr, tpr, _ = mtr.roc_curve(labels[aux], scores[aux])
+        pmiss = 1 - tpr
+        
+        yield key, min(map(lambda a: 100 * a[0] + a[1], zip(fpr, pmiss)))
+
+def det_graph(scores, labels, trial_set, marker):
+    #p_misses(thresh=t) + (100 * fpr(thresh=t))
+    fpr, tpr, _ = mtr.roc_curve(labels[trial_set == "eval"], scores[trial_set == "eval"])
+    pmiss = 1 - tpr
+    
+    plt.loglog(fpr, tpr, marker)
+
 def main():
     # load ivector ids, durations and ivectors (as row vectors)
     dev_ids, dev_durations, dev_ivec = load_ivectors("data/dev_ivectors.csv")
@@ -81,26 +254,16 @@ def main():
     
     # center and whiten all i-vectors
     dev_ivec = np.dot(dev_ivec - m, W.transpose())
-    model_ivec = np.dot(model_ivec - m, W.transpose())
     test_ivec = np.dot(test_ivec - m, W.transpose())
+    model_ivec = np.dot(model_ivec - m, W.transpose())
     
     # project all i-vectors into unit sphere
-    dev_ivec /= np.sqrt(np.sum(dev_ivec ** 2, axis=1))[:, np.newaxis]
     model_ivec /= np.sqrt(np.sum(model_ivec ** 2, axis=1))[:, np.newaxis]
+    dev_ivec /= np.sqrt(np.sum(dev_ivec ** 2, axis=1))[:, np.newaxis]
     test_ivec /= np.sqrt(np.sum(test_ivec ** 2, axis=1))[:, np.newaxis]
     
-    # create a speaker model by taking mean over i-vectors
-    # from that speaker (in this case each should have 5)
-    avg_model_ivec = np.zeros((len(np.unique(model_key)), model_ivec.shape[1]))
-    avg_model_names = []
-    
-    for i, key in enumerate(np.unique(model_key)):
-        avg_model_ivec[i] = np.mean(model_ivec[model_key == key], axis=0)
-        avg_model_names.append(key)
-    
-    # project the avg model i-vectors into unit sphere
-    avg_model_ivec /= np.sqrt(np.sum(avg_model_ivec ** 2, axis=1)) \
-        [:, np.newaxis]
+    avg_model_ivec, avg_model_names = \
+        generate_models(model_ivec, model_key, m, W)
     
     # compute inner product of all avg model ivectors and test ivectors
     # since everything is already unit length this is the cosine distance
@@ -112,9 +275,63 @@ def main():
     # The above can be computed quickly doing matrix multiplication but
     # note that each trial is independent and scores cannot computed based
     # on other avg model ivectors nor scores adjusted based on other scores
-    scores = np.dot(avg_model_ivec, test_ivec.transpose())
+    
+    #scores = np.dot(avg_model_ivec, test_ivec.transpose())
+    scores = np.dot(test_ivec, avg_model_ivec.transpose())
+    #scores = np.divide(scores - scores.mean(axis=1)[:, np.newaxis], scores.std(axis=1)[:, np.newaxis])
+    #z_aux = np.dot(avg_model_ivec, avg_model_ivec.transpose())
+    
+    trial_target, trial_set = load_trial_keys("data/ivec14_sre_trial_key_release.tsv")
+    avg_model_names_indexes = np.argsort(np.array(avg_model_names))
+    test_ids_indexes = np.argsort(np.array(test_ids))
+    
+    print "No normalization"
+    
+    score_list = scores[test_ids_indexes].T[avg_model_names_indexes].reshape(-1)
+    #det_graph(score_list, trial_target, trial_set, "r.")
+    print "EER", list(eer(score_list, trial_target, trial_set))
+    print "minDCF", list(min_dcf(score_list, trial_target, trial_set))
+    print
+    
+    use_t_norm = True
+    use_z_norm = True
+    use_lsh = True
+    
+    for n_norm in [10, 25, 50, 100, 250, 500, 1000, -1]:
+        for use_lsh in [True, False]:
+            n_scores = scores.copy()
+            
+            begin = time.time()
+            
+            if use_z_norm:
+                z_norm(n_scores, avg_model_ivec, avg_model_names, model_ivec,
+                       model_key, n_norm=n_norm, use_lsh=use_lsh)
+            
+            if use_t_norm:
+                t_norm(n_scores, avg_model_ivec, test_ivec, n_norm=n_norm,
+                       use_lsh=use_lsh)
+            
+            end = time.time()
+            
+            print "ZT-norm (%d, %s)" % (n_norm, use_lsh)
+            print "Time", (end - begin)
+            
+            n_score_list = n_scores[test_ids_indexes]. \
+                T[avg_model_names_indexes].reshape(-1)
+            print "EER", list(eer(n_score_list, trial_target, trial_set))
+            print "minDCF", list(min_dcf(n_score_list, trial_target, trial_set))
+            print
+            
+            sys.stdout.flush()
+            
+            det_graph(score_list, trial_target, trial_set, "r-")
+            det_graph(n_score_list, trial_target, trial_set, "b-")
+            plt.xlim(1e-3, 1)
+            plt.ylim(1e-1, 1)
+            plt.savefig("zt_norm_%d_%s.png" % (n_norm, use_lsh))
+            plt.cla()
 
-
+    
     # format and order scores for submission
     #    the format is a single score as ASCII formated float
     #    per line ordered lexigraphically by model_name then test_id
@@ -125,16 +342,29 @@ def main():
     #    scores (should be 1306 x 9634 for a total of 12,582,004 scores)
     #
     # so sort and output to a bz2 file
-    avg_model_names_indexes = np.argsort(np.array(avg_model_names))
-    test_ids_indexes = np.argsort(np.array(test_ids))
-    
-    f = bz2.BZ2File('myscores.txt.bz2', 'w')
-    
+    #aux1 = np.empty(avg_model_names_indexes.shape, dtype=np.int)
+    #aux1[avg_model_names_indexes] = np.arange(avg_model_names_indexes.size)
+    """
+    #f = bz2.BZ2File('myscores.txt.bz2', 'w')
+    f = open('myscores_norm.txt', 'w')
+    #print aux1[aux[test_ids_indexes[:20]]]
     for i in avg_model_names_indexes:
         for j in test_ids_indexes:
-            f.write('%f\n' % scores[i,j])
+            f.write('%f\n' % scores[j, i])
     
     f.close()
+    
+    print "ZT-norm"
+    
+    score_list = scores[test_ids_indexes].T[avg_model_names_indexes].reshape(-1)
+    det_graph(score_list, trial_target, trial_set, "b.")
+    print "EER", list(eer(score_list, trial_target, trial_set))
+    print "minDCF", list(min_dcf(score_list, trial_target, trial_set))
+    
+    #plt.xlim(1e-5, .1)
+    #plt.ylim(1e-5, .1)
+    plt.show()
+    """
 
 if __name__=='__main__':
     main()
--- hashtable.py
+++ hashtable.py
@@ -1,101 +1,115 @@
 import numpy as np
 import random
-from pandas import Series
-from random import sample
-import scipy.spatial.distance as scdist
+import pandas
+#import scipy.spatial.distance as scdist
 import datetime
-from timer import Timer
 
-class Lshtable:
-	def __init__(self,numkeys,lshfun,data):
-		self.numK = numkeys
-		self.numT = len(lshfun)
-		self.f = lshfun
-		self.data = data
-		self.table = Series({l:Series({k:[] for k in range(self.numK)}) for l in range(self.numT)})
-	
-	def mapindex(self,key):
-		vecfun=np.vectorize(lambda fun:fun.hash(key))
-		return vecfun(self.f)
+#from timer import Timer
 
-	def indexdata(self):
-		for i in range(len(self.data)):
-			e=self.data[i]
-			L=0
-			t1=datetime.datetime.now()
-			for ind in self.mapindex(e):
-				ind = ind % self.numK
-				self.table[L][ind].append(i)
-				L+=1
-			t2=datetime.datetime.now()
-			print "time point="+str(t2-t1)
-	def candidateset(self,qpoint):
-		L=0
-		result = []
-		for ind in self.mapindex(qpoint):
-			ind = ind % self.numK
-                        result.extend(self.table[L][ind])
-			L+=1
-		return np.unique(result)
+class LSHTable:
+    
+    def __init__(self, numkeys, lshfun, data):
+        self.numK = numkeys
+        self.numT = len(lshfun)
+        self.f = lshfun
+        self.data = data
+        self.table = pandas.Series({l: pandas.Series({k: [] for k in
+                                                      xrange(self.numK)})
+                                    for l in xrange(self.numT)})
+    
+    def map_index(self, key):
+        vecfun = np.vectorize(lambda fun: fun.hash(key))
+        
+        return vecfun(self.f)
+
+    def index_data(self):
+        for i in xrange(len(self.data)):
+            e = self.data[i]
+            
+            for L, ind in enumerate(self.map_index(e)):
+                ind = ind % self.numK
+                self.table[L][ind].append(i)
+    
+    def candidate_set(self, qpoint):
+        result = []
+        
+        for L, ind in enumerate(self.map_index(qpoint)):
+            ind = ind % self.numK
+            result.extend(self.table[L][ind])
+        
+        return np.unique(result)
 
 class LSHkNN:
-	def __init__(self,distfun,lshtble):
-		self.dist = distfun
-		self.table = lshtble
-
-	def initVoronoiLSH(L,k,data):
-		funlst = []
-		dist = scdist.euclidean
-		print "data[0] "
-		print data[0]
-		for i in range(L):
-			idx = random.sample(data,k)
-			
-			#print idx 
-			funlst.append(VoronoiLSH(idx,dist))
-		table = Lshtable(k,np.array(funlst),data)
-		knn  = LSHkNN(dist,table)
-		with Timer() as t:
-			table.indexdata()	
-		print "=> elasped indexdata: %s s" % t.secs
-		return knn
-
-    	initVoronoiLSH = staticmethod(initVoronoiLSH)
-
-
-	def candidateDistance(self,query):
-		fcalcdist=lambda ind:(self.dist(self.table.data[ind],query),ind)
-		return [fcalcdist(i) for i in self.table.candidateset(query)]
-	def kNNQuery(self,k,query):
-	        distlst = self.candidateDistance(query)
-		distlst.sort()
-		return distlst[:k]
-	def rangeQuery(self,radi,query):
-		distlst = candidateDistance(query)
-		return [(x,y) for (x,y) in distlst if x < radi]
+    
+    @staticmethod
+    def create_voronoi_lsh(L, k, data):
+        funlst = []
+        #dist = scdist.cosine #euclidean
+        dist = lambda a, b: 1 - np.dot(a, b)
+        
+        for i in xrange(L):
+            idx = random.sample(data, k)
+            funlst.append(VoronoiLSH(idx, dist))
+        
+        table = LSHTable(k, np.array(funlst), data)
+        knn = LSHkNN(dist, table)
+        
+        #with Timer() as t:
+        table.index_data()
+        
+        #print "=> elasped index_data: %s s" % t.secs
+        
+        return knn
+    
+    def __init__(self, distfun, lshtble):
+        self.dist = distfun
+        self.table = lshtble
+    
+    def candidate_distance(self, query):
+        #fcalcdist = lambda ind: (self.dist(self.table.data[ind], query), ind)
+        #
+        #return [fcalcdist(i) for i in self.table.candidate_set(query)]
+        candidates = self.table.candidate_set(query)
+        
+        return self.dist(self.table.data[candidates, :],
+                         query[:, np.newaxis])[:, 0], candidates
+    
+    def knn_query(self, k, query):
+        distlst, indices = self.candidate_distance(query)
+        idxs = np.argsort(distlst)[:k]
+        
+        return distlst[idxs], indices[idxs]
+    
+    def range_query(self, radi, query):
+        distlst = self.candidate_distance(query)
+        
+        return [(x, y) for (x, y) in distlst if x < radi]
 
 class VoronoiLSH:
-	def __init__(self,anchorpoints,dist):
-		self.dist=dist
-		self.C = anchorpoints
-	def hash(self,q):
-		t1=datetime.datetime.now()
-		#r = scdist.cdist(np.array([q]), self.C, 'euclidean',p=1).argmin
-		
-		r = np.array([self.dist(x,q) for x in self.C]).argmin()
-		#fdist=np.vectorize(lambda x:self.dist(x,q))
-		#r = fdist(self.C).argmin()
-		print "time hash="+str(datetime.datetime.now()-t1)
-		return r
+    
+    def __init__(self, anchorpoints, dist):
+        self.dist = dist
+        self.C = anchorpoints
+    
+    def hash(self, q):
+        #r = scdist.cdist(np.array([q]), self.C, 'euclidean',p=1).argmin
+        r = np.argmin([self.dist(x, q) for x in self.C])
+        #fdist=np.vectorize(lambda x:self.dist(x,q))
+        #r = fdist(self.C).argmin()
+        
+        return r
+
 class E2LSH:
-	def __init__(self,d,w,m):
-		self.d = d
-		self.w = w
-		self.m = m
+    
+    def __init__(self, d, w, m):
+        self.d = d
+        self.w = w
+        self.m = m
 
 class CossineLSH:
-	def __init__(self,d,numplanes,m):
-		self.d = d
-		self.b = numplanes
-		self.m = m 
+    
+    def __init__(self, d, numplanes, m):
+        self.d = d
+        self.b = numplanes
+        self.m = m 
 
